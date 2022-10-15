f75e16b00e3ddfacf4c7b426eb8aee64f9769fc6
Lucas Cabral <lucascabral6008@gmail.com>
2022-10-15 15:49:46 -0300

    adding firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {auth} from "..firebase";
+import {useAuthState} from "react-firebase-hooks/auth"; 
+
+
+const style = {
+    nav:`bg-gray-800 h-20 flex justify-between items-center p-4`,
+    heading:`text-white text-3xl`
+}
 
 const Navbar = () => {
 
-    const style = {
-        nav:`bg-gray-800 h-20 flex justify-between items-center p-4`,
-        heading:`text-white text-3xl`
-    }
-
+    const [user] = useAuthState(auth); 
 
   return (
     <div className={style.nav}>
